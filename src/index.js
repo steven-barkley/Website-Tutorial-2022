63ba3d986be728beaf17def79b28265bc947f5ae
Steven Barkley <steven.barkley@outlook.com>
2022-03-26 18:04:50 -0400

    Added a index.css and made changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 // Need to inject component functions Greetings into the index.html root need to use//
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 // ReactDom uses a renderMethod ... used at end of file
-
+import '../src/index.css';
 // We can create functions however we like function() or arrow function => *//
 // Stateless functional component
 // JSX Rules
@@ -40,17 +40,60 @@ import ReactDOM from 'react-dom'
 //Pass the Greeting <JSX/> into the render method
 
 // Nested Components, React Tools
-function Greeting() {
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   )
+// }
+
+// const Person = () => <h1>Steven Barkley is a person</h1>
+// const Message = () => <p>This is a message in JSX</p>
+
+// ReactDOM.render( <Greeting />, document.getElementById( 'root' ) )
+
+// //Pass JSX as <i></i> or <i/>
+
+//CSS
+// ./ mean same folder reference
+
+
+function BookList() {
   return (
-    <div>
-      <Person /><Person></Person>
-      <p>This is my Responsive REACT UX Portfolio</p>
-    </div>
-  )
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+const Book = () => {
+  return <article>
+    <Image></Image>
+    <Author />
+    <Title />
+  </article>
+}
+const Image = () => {
+  return <img
+    src='https://images-na.ssl-images-amazon.com/images/I/51h4pfU8jwL._SX436_BO1,204,203,200_.jpg'
+  />
+};
+const Author = () => {
+  return <h4>
+    Amelia Hepworth
+  </h4>
+};
+const Title = () => {
+  return <h1>I-Love-You-Moon-Back</h1>
+}
+ReactDOM.render( <BookList />, document.getElementById( 'root' ) )
 
-const Person = () => <h1>Steven Barkley</h1>
-
-ReactDOM.render( <Greeting />, document.getElementById( 'root' ) )
-
-//Pass JSX as <i></i> or <i/>
+// //Pass JSX as <i></i> or <i/>
