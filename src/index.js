fe24cfdb3e7b0930fc372016f0d6037eeb41420a
Steven Barkley <steven.barkley@outlook.com>
2022-03-26 02:31:37 -0400

    Added and commited old changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+// Need to inject component functions Greetings into the index.html root need to use//
+import ReactDOM from 'react-dom'
+// ReactDom uses a renderMethod ... used at end of file
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// We can create functions however we like function() or arrow function => *//
+// Stateless functional component
+// JSX Rules
+// div / Section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// Closs every element
+// formatting
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// function Greeting() {
+//   return (
+//     <div onClick>
+//       <h1>Passing JSX into the root UI.</h1>
+//       <ul>
+//         <li href='#'>formatting</li>
+//         <li href='#'>use camelCase for html attribute</li>
+//         <li href='#'>div / Section / article or Fragment</li>
+//         <li href='#'>className instead of class</li>
+//       </ul>
+//       <img src='' alt='' />
+//       <input type='text' name='' id='' />
+//     </div>
+//   )
+// }
+
+// const Greeting_Complex = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement( 'h3', {}, 'Passing JSX into the Root UI the hard way' )
+//   );
+// };
+
+//Pass the Greeting <JSX/> into the render method
+
+// Nested Components, React Tools
+function Greeting() {
+  return (
+    <div>
+      <Person /><Person></Person>
+      <p>This is my Responsive REACT UX Portfolio</p>
+    </div>
+  )
+}
+
+const Person = () => <h1>Steven Barkley</h1>
+
+ReactDOM.render( <Greeting />, document.getElementById( 'root' ) )
+
+//Pass JSX as <i></i> or <i/>
